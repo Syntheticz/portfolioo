6ea129982aadb40b73e9e5ab437689fd3c9ef496
Syntheticz <guiangphilip08@gmail.com>
2022-09-15 16:11:20 +0800

    please work

--- next.config.js
+++ next.config.js
@@ -1,27 +1,19 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
     MONGO_URI: 'mongodb+srv://User:lX7gXvlOya744gW3@clusterphobic.b8mshcc.mongodb.net/?retryWrites=true&w=majority'
   },
-}
-
-module.exports = nextConfig
-
-module.exports = {
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
+  source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "https://www.philguiang.ga" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  }
-};
+  ]
+}
+
+module.exports = nextConfig
+
